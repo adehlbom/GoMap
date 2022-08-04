32a7d12d4da83d2af68486b77b0069fe79030ab6
Anders Dehlbom <anders.dehlbom@gmail.com>
2022-08-05 01:12:02 +0200

    added goroutines

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
-	"github.com/thediveo/netdb"
 	"golang.org/x/exp/slices"
 )
 
@@ -13,56 +13,59 @@ import (
 var helpvar int
 
 func main() {
-	init_script()
-	checkFlags()
 
-}
-func init_script() {
-
-}
-func tcp_small() {
-	var successful_ports []int
-	arr := [5]int{20, 22, 80, 243, 3306}
-	for i := 0; i < len(arr); i++ {
-		address := fmt.Sprintf("192.168.0.104:%d", arr[i])
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			//fmt.Printf("Port %d is closed\n", arr[i])
-			continue
-		} else {
-			successful_ports = append(successful_ports, arr[i])
-
-		}
-		conn.Close()
-		//fmt.Printf("Port %d open\n", arr[i])
-		what_service := netdb.ServiceByPort(arr[i], "tcp")
-		fmt.Println(fmt.Sprint(what_service.Port) + " " + what_service.Name + " OPEN")
-	}
-
-}
-func tcp_full() {
-	var successful_ports []int
-	for i := 1; i < 65535; i++ {
-		address := fmt.Sprintf("192.168.0.104:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			//fmt.Printf("Port %d is closed\n", arr[i])
-			continue
-		} else {
-			successful_ports = append(successful_ports, i)
-
-		}
-		conn.Close()
-		//fmt.Printf("Port %d open\n", arr[i])
-		what_service := netdb.ServiceByPort(i, "tcp")
-		fmt.Println(fmt.Sprint(what_service.Port) + " " + what_service.Name + " OPEN")
+	activeThreads := 0
+	channel := make(chan bool)
+	ip_address := "192.168.0.104"
+	end_port := checkFlags()
+	for port := 0; port <= end_port; port++ {
+		go tcp_scan(ip_address, port, channel)
+		activeThreads++
 
 	}
+	for activeThreads > 0 {
+		<-channel
+		activeThreads--
+
+	}
+	fmt.Println("All done VÄlkommen åter")
+
 }
-func checkFlags() {
+
+func checkFlags() int {
 	if slices.Contains(os.Args, "-s") {
-		tcp_small()
+		return 1024
 	} else if slices.Contains(os.Args, "-f") {
-		tcp_full()
+		return 65535
 	}
+	return 0
+}
+
+func tcp_scan(ip_address string, port int, channel chan bool) {
+
+	address := fmt.Sprintf("%s:%d", ip_address, port)
+	conn, err := net.Dial("tcp", address)
+
+	if err != nil {
+		//fmt.Printf("Port %d is closed\n", arr[i])
+		channel <- true
+		return
+	}
+	buffer := make([]byte, 4096)
+	conn.SetReadDeadline(time.Now().Add(time.Second * 5))
+	numbytesread, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println(err)
+		channel <- true
+		return
+	}
+	conn.Close()
+	//log.Printf("Banner from port %d\n%s\n", successful_ports[i], buffer[0:numbytesread])
+	fmt.Printf("Port %d open\n", port)
+	fmt.Println(string(buffer[0:numbytesread]))
+	fmt.Println("------------------")
+
+	//what_service := netdb.ServiceByPort(i, "tcp")
+	//fmt.Println(fmt.Sprint(what_service.Port) + " " + what_service.Name + " OPEN")
+
 }
