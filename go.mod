99daa23d4197fb02e1792d7c83ba138ae890123f
Anders Dehlbom <anders.dehlbom@gmail.com>
2022-08-09 22:52:22 +0200

    update

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module hello
 go 1.18
 
 require (
-	github.com/google/gopacket v1.1.19 // indirect
-	github.com/thediveo/netdb v0.9.1 // indirect
-	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
 )
+
+require golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
--- main.go
+++ main.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/exp/slices"
 )
-
-// vars
-var helpvar int
 
 func main() {
 
 	activeThreads := 0
 	channel := make(chan bool)
-	ip_address := "192.168.0.104"
+	ip_address := "192.168.1.1"
 	end_port := checkFlags()
 	for port := 0; port <= end_port; port++ {
 		go tcp_scan(ip_address, port, channel)
@@ -44,7 +43,7 @@ func checkFlags() int {
 func tcp_scan(ip_address string, port int, channel chan bool) {
 
 	address := fmt.Sprintf("%s:%d", ip_address, port)
-	conn, err := net.Dial("tcp", address)
+	conn, err := net.DialTimeout("tcp", address, 10*time.Second)
 
 	if err != nil {
 		//fmt.Printf("Port %d is closed\n", arr[i])
@@ -63,9 +62,31 @@ func tcp_scan(ip_address string, port int, channel chan bool) {
 	//log.Printf("Banner from port %d\n%s\n", successful_ports[i], buffer[0:numbytesread])
 	fmt.Printf("Port %d open\n", port)
 	fmt.Println(string(buffer[0:numbytesread]))
+	if strings.Contains(strings.ToLower(string(buffer[0:numbytesread])), "ssh") {
+		test_ssh(ip_address, port)
+	}
 	fmt.Println("------------------")
 
 	//what_service := netdb.ServiceByPort(i, "tcp")
 	//fmt.Println(fmt.Sprint(what_service.Port) + " " + what_service.Name + " OPEN")
+
+}
+func test_ssh(ip_address string, port int) {
+	fmt.Println("Testing to connect to SSH server...")
+	var hostKey ssh.PublicKey
+
+	config := &ssh.ClientConfig{
+		User: "",
+		Auth: []ssh.AuthMethod{
+			ssh.Password(""),
+		},
+		HostKeyCallback: ssh.FixedHostKey(hostKey),
+	}
+	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", ip_address, port), config)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	conn.Close()
 
 }
